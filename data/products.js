80c23171a70e3ee2b2374f90208c0b975708ddd5
Thomas Dejardins <tdejardins@gmail.com>
2023-01-27 11:41:38 +0100

    feat(products.js): add products data

---
+++ data/products.js
@@ -0,0 +1,234 @@
+const christmasProducts = [
+  {
+    title: "Les chocolats (16 unités)",
+    price: 62,
+    description: "Astrid&Gastón dans un coffret cadeau 16 unités.",
+    image: "HRWT5nDamtmtnP4Mn-350-350.webp",
+  },
+  {
+    title: "Les chocolats (9 unités)",
+    price: 42,
+    description: "Astrid&Gastón dans un coffret cadeau 9 unités.",
+    image: "YEn4W4LxgDCDHQJB5-350-350.webp",
+  },
+  {
+    title: "Les chocolats (4 unités)",
+    price: 24,
+    description: "Astrid&Gastón dans un coffret cadeau 4 unités.",
+    image: "4sKYoLMKYecYjw64x-350-350.webp",
+  },
+  {
+    title: "Bisous d'une fille x 16 - Noël",
+    price: 62,
+    description:
+      "Saveurs assorties, chicha, vanille, cappuccino, fruit de la passion.",
+    image: "pPgWxbkroCyYknFQo-350-350.webp",
+  },
+  {
+    title: "Bisous d'une fille x 9 - Noël",
+    price: 44,
+    description:
+      "Saveurs assorties, chicha, vanille, cappuccino, fruit de la passion.",
+    image: "7uS6C743XdCHovMuY-350-350.webp",
+  },
+  {
+    title: "Bisous d'une fille x 4 - Noël",
+    price: 24,
+    description:
+      "Saveurs assorties, chicha, vanille, cappuccino, fruit de la passion.",
+    image: "zNxmNjxNhyNPyZu9h-350-350.webp",
+  },
+  {
+    title: "L'Inconditionnel - Noël",
+    price: 96,
+    description:
+      "Noix de pécan, noix, amandes, caramel salé, mousse au chocolat Tumbes 60% et éclats de cacao.",
+    image: "DdpL7aWKhJKF4JMaW-350-350.webp",
+  },
+  {
+    title: "Le Nostalgique - Noël",
+    price: 96,
+    description:
+      "Onctueux et élégant. Souvenirs de tarte au citron de notre enfance, cœur de caramel liquide baignant un léger biscuit au cacao, croustillant de gianduia et chantilly au chocolat blanc.",
+    image: "pmzaxoScoDgyxnGT3-350-350.webp",
+  },
+  {
+    title: "Tarte au Chocolat - Noël",
+    price: 96,
+    description:
+      "Noix de pécan, noix, amandes, caramel salé, mousse au chocolat Tumbes 60% et éclats de cacao.",
+    image: "DpEAyWDKwPpyyLZoJ-350-350.webp",
+  },
+  {
+    title: "Tarte au fromage",
+    price: 59,
+    description: "Au caramel, et éclats croustillants et amandes.",
+    image: "jLJGAqfC5qsE5RYRN-350-350.webp",
+  },
+];
+
+const iceCreams = [
+  {
+    title: "Glace à la vanille",
+    price: 59,
+    description: "Fabriqué à partir de gousses de vanille.",
+    image: "",
+  },
+  {
+    title: "Glace au yaourt",
+    price: 56,
+    description: "Avec compote de sureau et fruits de la passion.",
+    image: "",
+  },
+  {
+    title: "Glace au chocolat",
+    price: 59,
+    description:
+      "Glace gourmande crémeuse, noix de pécan, touches de ganache et de chocolat.",
+    image: "",
+  },
+  {
+    title: "Glace au cacao d'Amazonie 70%",
+    price: 59,
+    description: "Fabriqué à partir de fromages andins.",
+    image: "SCRevHHAYZBz298hC-350-350.webp",
+  },
+  {
+    title: "Glace Lucuma au caramel",
+    price: 58,
+    description: "Aux arômes de café et de morceaux de brownie.",
+    image: "6rFjdR9ypwF5M2ZLs-350-350.webp",
+  },
+  {
+    title: "Glace de panna",
+    price: 58,
+    description: "Aux éclats de nougat et au miel de figue.",
+    image: "",
+  },
+  {
+    title: "Sorbet aux fraises",
+    price: 39,
+    description: "Avec compote de fraises et lait concentré.",
+    image: "BTJZG5DD8WZ5HcHY3-350-350.webp",
+  },
+  {
+    title: "Sorbet à la figue de barbarie rouge",
+    price: 44,
+    description: "Avec le meilleur Thon de la saison.",
+    image: "Eh4eM9atroJgjJ9Q5-350-350.webp",
+  },
+  {
+    title: "Sorbet citron",
+    price: 44,
+    description: "Aux pommes confites et basilic.",
+    image: "9c6tnZr46CtgJrYcP-350-350.webp",
+  },
+  {
+    title: "Sorbet mangue et fruit de la passion",
+    price: 44,
+    description: "Avec compote de carambole confite.",
+    image: "Eh4eM9atroJgjJ9Q5-350-350.webp",
+  },
+];
+
+const pies = [
+  {
+    title: "Tarte au fromage",
+    price: 59,
+    description: "Au caramel, et éclats croustillants et amandes.",
+    image: "jLJGAqfC5qsE5RYRN-350-350.webp",
+  },
+];
+
+const cakes = [
+  {
+    title: "L'Inconditionnel",
+    price: 62,
+    description:
+      "Brownie moelleux aux pacanes et graines de sésame, lan au chocolat aux arômes de kion, génoise au cacao et mousse au chocolat.",
+    image: "8jN2ZroMCw3ydvCTg-350-350.webp",
+  },
+];
+
+const confectioneries = [
+  {
+    title: "Les chocolats (16 unités)",
+    price: 96,
+    description: "Astrid&Gastón dans un coffret cadeau 16 unités.",
+    image: "HRWT5nDamtmtnP4Mn-350-350.webp",
+  },
+  {
+    title: "Les chocolats (9 unités)",
+    price: 42,
+    description: "Astrid&Gastón dans un coffret cadeau 9 unités.",
+    image: "YEn4W4LxgDCDHQJB5-350-350.webp",
+  },
+  {
+    title: "Les chocolats (4 unités)",
+    price: 24,
+    description: "Astrid&Gastón dans un coffret cadeau 4 unités.",
+    image: "4sKYoLMKYecYjw64x-350-350.webp",
+  },
+  {
+    title: "Les chocolats (25 unités)",
+    price: 98,
+    description: "Astrid&Gastón dans un coffret cadeau 25 unités.",
+    image: "HRWT5nDamtmtnP4Mn-350-350.webp",
+  },
+  {
+    title: "Coffret de macarons assortis (9 unités)",
+    price: 43,
+    description:
+      "Saveurs assorties selon arrivage : Lucuma et Café, Gianduia et Citron, Cacahuète et Ananas, Mandarine et Cannelle, Chocolat, Caramel au sel, Vanille en gousse et sel, Fruit de la passion et Framboise, Banane - gourmandise pour 9 unités.",
+    image: "sn2Ld6u2RJ3HpcD8o-350-350.webp",
+  },
+  {
+    title: "L'inconditionnel",
+    price: 96,
+    description:
+      "Brownie moelleux aux pacanes et graines de sésame, lan au chocolat aux arômes de kion, génoise au cacao et mousse au chocolat.",
+    image: "8jN2ZroMCw3ydvCTg-350-350.webp",
+  },
+  {
+    title: "Coffret de macarons assortis (18 unités)",
+    price: 82,
+    description:
+      "Saveurs assorties selon arrivage : Lucuma et Café, Gianduia et Citron, Cacahuète et Ananas, Mandarine et Cannelle, Chocolat, Caramel au sel, Vanille en gousse et sel, Fruit de la passion et Framboise, Banane - gourmandise pour 18 unités.",
+    image: "Z9y8M4qCJnCoNNzZ9-350-350.webp",
+  },
+  {
+    title: "Sachet de bisous de fille x 4 unité",
+    price: 64,
+    description: "Saveurs chicha, vanille, cappuccino, fruit de la passion.",
+    image: "8faQEyYtk47GHbSM2-350-350.webp",
+  },
+  {
+    title: "Sachet de bisous de fille x 16 unité",
+    price: 128,
+    description: "Saveurs chicha, vanille, cappuccino, fruit de la passion.",
+    image: "KMmctgGALBCYSeeYz-350-350.webp",
+  },
+  {
+    title: "Boîte de chocolat (6 unités)",
+    price: 38,
+    description:
+      "Saveurs assorties, Classique, cerise, café, cacahuète, praliné et citron.",
+    image: "P6SfbrNrnqPxeuSTz-350-350.webp",
+  },
+  {
+    title: "Boîte de chocolat (12 unités)",
+    price: 69,
+    description:
+      "Saveurs assorties, chicha, vanille, cappuccino, fruit de la passion.",
+    image: "",
+  },
+  {
+    title: "Les coups de chocolat",
+    price: 119,
+    description:
+      "Negroni, Anís najar, Algarrobina, Mezcal, Irish coffee, Absentha, Macerado de ajíes, Aged Rum et Sazerac.",
+    image: "BFXSLWj8cAxSRbPTP-350-350.webp",
+  },
+];
+
+export { christmasProducts, iceCreams, pies, cakes, confectioneries };
